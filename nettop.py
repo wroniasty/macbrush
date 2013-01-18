0c8d0923cdd2cf0aadea2296255321722e7be113
Jakub Wroniecki <wroniasty@gmail.com>
2013-01-18 18:22:09 +0100

    top

--- nettop.py
+++ nettop.py
@@ -7,10 +7,10 @@ from collections import defaultdict
 import time
 import curses, curses.panel
 
-class CasaStatDevice(Agent):
+class IfStatDevice(Agent):
 
     def __init__(self, ip, interval=3):
-        super(CasaStatDevice, self).__init__(ip)
+        super(IfStatDevice, self).__init__(ip)
         self.target_ip = ip
         self.poll_interval = interval
         self.data = {}
@@ -34,22 +34,41 @@ class CasaStatDevice(Agent):
                 if_data['rate'] = if_data.get('rate', defaultdict(lambda : 0))
                 if_data['bitrate'] = if_data.get('bitrate', defaultdict(lambda : 0))
 
-                def calculate_rate(H1, H2):
-                    t1, v1 = H1
-                    t2, v2 = H2
-                    return max((v2 - v1) / (t2 - t1), 0)
 
+                self.data[index] = if_data
                 for stat_name in if_data['history']:
-                    if_data['history'][stat_name].append((ts, iff.get(stat_name, 0)))
-                    H = if_data['history'][stat_name]
-                    if len(H) >= 2:
-                        if_data['rate'][stat_name] = calculate_rate(H[-1], H[-2])
-                        if_data['bitrate'][stat_name] = if_data['rate'][stat_name] * 8;
+                    self.push_stat_data_point(if_data, stat_name, ts, iff.get(stat_name, 0))
+                    #if_data['history'][stat_name].append((ts, iff.get(stat_name, 0)))
+                    #H = if_data['history'][stat_name]
+                    #if len(H) >= 2:
+                    #    if_data['rate'][stat_name] = calculate_rate(H[-1], H[-2])
+                    #    if_data['bitrate'][stat_name] = if_data['rate'][stat_name] * 8;
 
                 #print index, if_data['Descr'], if_data['Speed'], if_data['rate']['InOctets']
-                self.data[index] = if_data
+
 
         reactor.callLater(self.poll_interval, self.run)
+
+    def push_stat_data_point(self, if_data, stat_name, timestamp, value):
+
+        def calculate_rate(H1, H2):
+            t1, v1 = H1
+            t2, v2 = H2
+            return max((v2 - v1) / (t2 - t1), 0)
+
+        try:
+            if_data['history'][stat_name].append((timestamp, value))
+            H = if_data['history'][stat_name]
+            if len(H) >= 2:
+                rate = if_data['rate'][stat_name] = calculate_rate(H[-1], H[-2])
+                if_data['bitrate'][stat_name] = rate * 8
+                return rate
+        except KeyError:
+            pass
+
+        return None
+
+
 
     def on_done(self, results):
         try:
@@ -60,7 +79,7 @@ class CasaStatDevice(Agent):
         except Exception, e:
             self.mapper_deferred.errback( e )
 
-class CursesStdIO:
+class CursesStdIO(object):
     """fake fd to be registered as a reader with the twisted reactor.
        Curses classes needing input should extend this"""
 
@@ -98,81 +117,7 @@ class Screen(CursesStdIO):
         self.current_type = 6
 
     def update(self, data):
-        self.rows, self.cols = self.stdscr.getmaxyx()
-        self.stdscr.clear()
-
-        self.stdscr.box()
-
-        self.available_types = [t for t in set([ t["Type"] for t in data.values() ]) ]
-        self.stdscr.addstr(self.rows-1, 1, `self.available_types`, curses.color_pair(1))
-
-        def ifOfType(data, ifType):
-            if not isinstance(ifType, list): ifType = [ ifType ]
-            items = [ item for item in data.values() if item["Type"] in ifType]
-            items.sort( lambda x,y: x["Index"] - y["Index"])
-            return items
-
-        def shortenText(txt, l):
-            if len(txt) > l:
-                txt = txt[0:l/2 - 2] + "...." + txt[-l/2 + 2:]
-            return txt
-
-        if 129 in self.available_types:
-            win = self.panels[129]
-            for idx, iff in enumerate(ifOfType(data,129)[:self.rows-4]):
-                row = idx + 1
-                col_off = 5
-                win.addstr(row, col_off + 0, shortenText(iff["Descr"],16), curses.color_pair(1) )
-                if iff["AdminStatus"] == 1:
-                    win.addstr(row, col_off + 18, "up", curses.color_pair(1) )
-                if (iff["bitrate"]["InOctets"] > 0):
-                    win.addstr(row, col_off + 23, "{: >3.1f}M".format(iff["bitrate"]["InOctets"] / 1000 / 1000), curses.color_pair(1) )
-                    win.addstr(row, col_off + 43, "{: >3d}%".format(int(iff['bitrate']["InOctets"] / iff['Speed'] * 100)), curses.color_pair(1) )
-
-                win.addstr(row, col_off + 29, "{: >5.1f}e".format(iff["rate"]["InErrors"]), curses.color_pair(1) )
-
-        if 128 in self.available_types:
-            win = self.panels[128]
-            for idx, iff in enumerate(ifOfType(data,128)[:self.rows-4]):
-                row = idx + 1
-                col_off = 2
-                win.addstr(row, col_off + 0, shortenText(iff["Descr"],16), curses.color_pair(1) )
-                if iff["AdminStatus"] == 1:
-                   win.addstr(row, col_off + 18, "up", curses.color_pair(1) )
-                if (iff["bitrate"]["OutOctets"] > 0):
-                    win.addstr(row, col_off + 23, "{: >3.1f}M".format(iff["bitrate"]["OutOctets"] / 1000 / 1000), curses.color_pair(1) )
-                    win.addstr(row, col_off + 43, "{: >3d}%".format(int(iff['bitrate']["OutOctets"] / iff['Speed'] * 100)), curses.color_pair(1) )
-
-        if 6 in self.available_types:
-            win = self.panels[6]
-            for idx, iff in enumerate(ifOfType(data,6)[:self.rows-4]):
-                row = idx + 1
-                col_off = 2
-                win.addstr(row, col_off + 0, shortenText(iff["Descr"],16), curses.color_pair(1) )
-                if iff["AdminStatus"] == 1:
-                    win.addstr(row, col_off + 18, "up", curses.color_pair(1) )
-
-                win.addstr(row, col_off + 23, "{: >3.1f}M    ".format(iff["bitrate"]["OutOctets"] / 1000 / 1000), curses.color_pair(1) )
-                win.addstr(row, col_off + 33, "{: >3.1f}M    ".format(iff["bitrate"]["InOctets"] / 1000 / 1000), curses.color_pair(1) )
-
-        self.panels[self.current_type].overwrite(self.stdscr)
-            #self.stdscr.addstr(idx+1, 0, `iff`, curses.color_pair(1))
-
-        self.type_names = {
-            6  : "Ethernet",
-            128 : "Downstream",
-            129 : "Upstream"
-        }
-        col = 1
-        for if_type in self.type_names.keys():
-            name =  self.type_names.get(if_type, None)
-            if not name: continue
-            if if_type == self.current_type: clr = 3
-            else: clr = 2
-            self.stdscr.addstr(1, col, name, curses.color_pair(clr))
-            col += len(name) + 2
-
-        self.stdscr.refresh()
+        pass
 
     def connectionLost(self, reason):
         self.close()
@@ -216,23 +161,6 @@ class Screen(CursesStdIO):
             if idx >= len(Ks): idx = 0
         self.current_type = Ks[idx]
 
-
-        #if c == curses.KEY_BACKSPACE:
-        #    self.searchText = self.searchText[:-1]
-
-        #elif c == curses.KEY_ENTER or c == 10:
-        #    self.addLine(self.searchText)
-        #    self.stdscr.refresh()
-
-        #else:
-        #    if len(self.searchText) == self.cols-2: return
-        #    self.searchText = self.searchText + chr(c)
-
-        #self.stdscr.addstr(self.rows-1, 0,
-        #    self.searchText + (' ' * (
-        #        self.cols-len(self.searchText)-2)))
-        #self.stdscr.move(self.rows-1, len(self.searchText))
-        #self.paintStatus(self.statusText + ' %d' % len(self.searchText))
         self.stdscr.refresh()
 
     def close(self):
@@ -243,26 +171,114 @@ class Screen(CursesStdIO):
         curses.echo()
         curses.endwin()
 
+
+
+class IfStatCursesViewer(Screen):
+
+    def __init__(self, if_stat, interval=1):
+        stdscr = curses.initscr()
+        super(IfStatCursesViewer, self).__init__(stdscr)
+
+        self.if_stat = if_stat
+        reactor.addReader(self)
+        self.display_task = task.LoopingCall ( self.update_display)
+        self.display_task.start(0.5)
+
+    def update_display(self):
+        data = self.if_stat.data
+        self.rows, self.cols = self.stdscr.getmaxyx()
+        self.stdscr.clear()
+
+        self.stdscr.box()
+
+        self.available_types = [t for t in set([ t["Type"] for t in data.values() ]) ]
+        self.stdscr.addstr(self.rows-1, 1, `self.available_types`, curses.color_pair(1))
+
+        def ifOfType(data, ifType):
+            if not isinstance(ifType, list): ifType = [ ifType ]
+            items = [ item for item in data.values() if item["Type"] in ifType]
+            items.sort( lambda x,y: x["Index"] - y["Index"])
+            return items
+
+        def shortenText(txt, l):
+            if len(txt) > l:
+                txt = txt[0:l/2 - 2] + "...." + txt[-l/2 + 2:]
+            return txt
+
+        map (lambda p: p.clear(), self.panels.values())
+
+        col_off = 5
+        row_off = 2
+        data_slice = slice(0,self.rows-5)
+
+        if 129 in self.available_types:
+            win = self.panels[129]
+            for idx, iff in enumerate(ifOfType(data,129)[data_slice]):
+                row = idx + row_off
+                win.addstr(row, col_off + 0, shortenText(iff["Descr"],16), curses.color_pair(1) )
+                if iff["AdminStatus"] == 1:
+                    win.addstr(row, col_off + 18, "up", curses.color_pair(1) )
+                if (iff["bitrate"]["InOctets"] > 0):
+                    win.addstr(row, col_off + 23, "{: >3.1f}M".format(iff["bitrate"]["InOctets"] / 1000 / 1000), curses.color_pair(1) )
+                    win.addstr(row, col_off + 43, "{: >3d}%".format(int(iff['bitrate']["InOctets"] / iff['Speed'] * 100)), curses.color_pair(1) )
+
+                win.addstr(row, col_off + 29, "{: >5.1f}e".format(iff["rate"]["InErrors"]), curses.color_pair(1) )
+
+        if 128 in self.available_types:
+            win = self.panels[128]
+            for idx, iff in enumerate(ifOfType(data,128)[data_slice]):
+                row = idx + row_off
+                win.addstr(row, col_off + 0, shortenText(iff["Descr"],16), curses.color_pair(1) )
+                if iff["AdminStatus"] == 1:
+                    win.addstr(row, col_off + 18, "up", curses.color_pair(1) )
+                if (iff["bitrate"]["OutOctets"] > 0):
+                    win.addstr(row, col_off + 23, "{: >3.1f}M".format(iff["bitrate"]["OutOctets"] / 1000 / 1000), curses.color_pair(1) )
+                    win.addstr(row, col_off + 43, "{: >3d}%".format(int(iff['bitrate']["OutOctets"] / iff['Speed'] * 100)), curses.color_pair(1) )
+
+        if 6 in self.available_types:
+            win = self.panels[6]
+            for idx, iff in enumerate(ifOfType(data,6)[data_slice]):
+                row = idx + row_off
+                win.addstr(row, col_off + 0, shortenText(iff["Descr"],16), curses.color_pair(1) )
+                if iff["AdminStatus"] == 1:
+                    win.addstr(row, col_off + 18, "up", curses.color_pair(1) )
+
+                win.addstr(row, col_off + 23, "{: >3.1f}M    ".format(iff["bitrate"]["OutOctets"] / 1000 / 1000), curses.color_pair(1) )
+                win.addstr(row, col_off + 33, "{: >3.1f}M    ".format(iff["bitrate"]["InOctets"] / 1000 / 1000), curses.color_pair(1) )
+
+        self.panels[self.current_type].overwrite(self.stdscr)
+        #self.stdscr.addstr(idx+1, 0, `iff`, curses.color_pair(1))
+
+        self.type_names = {
+            6  : "Ethernet",
+            128 : "Downstream",
+            129 : "Upstream"
+        }
+        col = 1
+        for if_type in self.type_names.keys():
+            name =  self.type_names.get(if_type, None)
+            if not name: continue
+            if if_type == self.current_type: clr = 3
+            else: clr = 2
+            self.stdscr.addstr(1, col, name, curses.color_pair(clr))
+            col += len(name) + 2
+
+        self.stdscr.refresh()
+
+
 def main(argv):
 
 
-    topper = CasaStatDevice("10.1.2.1")
+    topper = IfStatDevice("10.1.0.5")
     topper.run()
-
-    stdscr = curses.initscr()
-    screen = Screen(stdscr)
-    def update_display(topper):
-        screen.update(topper.data)
-    reactor.addReader(screen)
-    display_task = task.LoopingCall ( update_display, topper )
-    display_task.start(0.5)
+    screen = IfStatCursesViewer(topper)
 
     try:
         reactor.run()
     except KeyboardInterrupt:
         reactor.stop()
     finally:
-        pass #screen.close()
+        screen.close()
 
 if __name__ == "__main__":
     import sys
